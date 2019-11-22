a13eb4f6cf6a63013b5b94819408b360827cd1aa
usager <usager@10.0.2.15>
2019-11-22 14:53:26 -0500

    version 1.1

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,6 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Bonjour "+args[1]+", "+args[0]);
+        System.out.println("Au revoir !!!");
     }
     
 }
